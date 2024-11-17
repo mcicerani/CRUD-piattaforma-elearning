09097b371ad694def8cc5f38f90c6a4d5b36675c
mcicerani <mcicerani@gmail.com>
2024-11-17 13:18:12 +0100

    connessione al database

---
+++ includes/config.php
@@ -0,0 +1,15 @@
+<?php
+//Connessione al database
+$host = "localhost";
+$user = "root";
+$password = "root";
+$db_name = "crud_db";
+
+$conn = new mysqli($host, $user, $password, $db_name);
+
+//Controllo errori di connessione
+if ($conn->connect_error) {
+    die("Errore di connessione: " . $conn->connect_error);
+}
+
+echo "Connessione riuscita";
